0976576ec82d1c9daa134b579181e01673cd4621
kobe sun <457892529@qq.com>
2024-01-25 20:59:17 +0800

    UPDATA

---
+++ futbin/xijianan.py
@@ -0,0 +1,44 @@
+from futbin import futbinPrice
+import time
+
+AthleticClub=[248550,171579,146947,231184,227950,191740,205157,216194]
+AtleticodeMa=[251445,204639,177413,232119,252324,258390]
+CAOsasuna=[188335,211022,224003,206590,193314,208137,238305,243976,225200]
+CadizCF=[219391,239207,146439,205590,259173,229788,199247,229628]
+DAlaves=[224415,226637,240953]
+FCBarcelona=[199564,199576,192638]
+GetafeCF=[223952,50570983,217940,234835,203843,240359,232665,239433,242201]
+GironaFC=[186537,190815,50536718,50576685,234060,201505]
+GranadaCF=[261204,50553689,251412,236679,183353,185020]
+RayoVallecano=[173030,211348,189472,243558,243559,203581]
+RCCelta=[240654,235997,50556733,201095,220673,240900,223061,50521338]
+RCDMallorca=[228805,193171,263396,219585,208620,226777,223710,241727,198451]
+RealBetis=[198141,171897,264698,195093,207566,210385,215699,203747,235945]
+RealMadrid=[264309,206585,222509]
+RealSociedad=[231627,233486,50565683,237681,226221,248148]
+SevillaFC=[234426,231628,168651,231416,208330,208777,255106,202651,50571465,146536]
+UDAlmeria=[50561628,245158,205976,219953,245598,271673,244316,205288,247500]
+UDLasPalmas=[264388,243478,220253,217731,246657]
+ValenciaCF=[247729,229010,246352,230578]
+VillarrealCF=[205192,201143,259377,157481,237221,206545,205566,235781,216549,24630]
+
+
+all=[AthleticClub,AtleticodeMa,CAOsasuna,CadizCF,DAlaves,FCBarcelona,GetafeCF,GironaFC,GranadaCF,RayoVallecano,
+    RCCelta,RCDMallorca,RealBetis,RealMadrid,RealSociedad,SevillaFC,UDAlmeria,UDLasPalmas,ValenciaCF,VillarrealCF]
+
+def futbinPrice_xijianan():
+    tmp = 0
+    for i in all:
+        tmp_tmp = 0
+        print("now is %d" % (tmp))
+        for k in i:
+            time.sleep(1)
+            tmp_tmp = tmp_tmp + futbinPrice(k)
+
+        tmp_tmp = tmp_tmp / len(i)
+
+        if (tmp_tmp > 800):
+            print("%d is ok %d" % (tmp,tmp_tmp))
+        tmp = tmp + 1
+if __name__ == '__main__':
+    futbinPrice_xijianan()
---
+++ futbin/xijianv.py
@@ -0,0 +1,38 @@
+from futbin import futbinPrice
+import time
+
+AthleticClub=[272131,241517,272138,272212,246286,272135,272123]
+AtleticodeMa=[264725,272114,265021,272115,272110,256302,233757]
+CATenerife=[233576,273547,272228,272235,246357]
+FCBarcelona=[268942,242896]
+GranadaCF=[275779]
+LevanteLP=[272159,272163]
+LevanteUD=[272020,272021,272012,272013,262459,271944,272004]
+MadridCFF=[246359,272079,246364,272064,272061,272067]
+RealMadridCF=[233155,259372,271945,227466,258576,264946,273153,267326,264865]
+RealSociedad=[276796,272200,272202,261843,272211]
+SevillaFC=[268993,272050,272047,272053,272056,261853,241182]
+SportingHuelva=[272251,272246]
+ValenciaCF=[272098,272089,272099,271943,272094,272065]
+VillarrealCF=[227195,272150]
+
+
+all=[AthleticClub,AtleticodeMa,CATenerife,FCBarcelona,GranadaCF,LevanteLP,LevanteUD,MadridCFF,
+    RealMadridCF,RealSociedad,SevillaFC,SportingHuelva,ValenciaCF,VillarrealCF]
+
+def futbinPrice_xijianv():
+    tmp = 0
+    for i in all:
+        tmp_tmp = 0
+        print("now is %d" % (tmp))
+        for k in i:
+            time.sleep(1)
+            tmp_tmp = tmp_tmp + futbinPrice(k)
+
+        tmp_tmp = tmp_tmp / len(i)
+
+        if (tmp_tmp > 800):
+            print("%d is ok %d" % (tmp,tmp_tmp))
+        tmp = tmp + 1
+if __name__ == '__main__':
+    futbinPrice_xijianv()
